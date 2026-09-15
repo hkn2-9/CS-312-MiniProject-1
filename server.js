cf11eae6b6766c9a7502a817b279168eedd28b06
hkn2-9 <hkn33@nau.edu>
2026-09-14 17:34:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("<h1>Hello<h1>");
 });
 
 app.listen(PORT, () => {
